f6a34591ac59f0509c7fbcbb186aa61022045694
BlueJayBerry <jaynwright2@gmail.com>
2024-08-31 08:17:32 -0500

    Solve problem with stack not showing
    This was caused by setting the upper limit of the for loop
    to a variable that was initialized at zero.
    This cause the stack to be empty on program execution.
    To solve this I had to set the upper limit to a new variable
    with the number of elements of the array

--- stack-with-pointers.c
+++ stack-with-pointers.c
@@ -12,13 +12,35 @@ void push(int x){
 }
 
 int pop(){
-    int result = Stack.array[Stack.count];
+    int result = Stack.array[Stack.count - 1];
     Stack.count--;
     return result;
 }
 
+void reverse(int* array, int numOfElements){
+    int i;
+    for(i = 0; i < numOfElements; i++){
+        push(array[i]);
+    }
+}
+
 int main(int argc, char argv[]){
+    int array[100] = {1,2,3,4};
     Stack.count = 0;
+    int numOfElements = 0;
+    int i;
+
+    for(i =0; i < 100; i++){
+        if(array[i]){ 
+            numOfElements++;
+        }
+    }
+
+    reverse(array, numOfElements);
+
+    for(i = 0; i < numOfElements; i++){
+        printf("%d ", pop());
+    }
 
     return 0;
 }
